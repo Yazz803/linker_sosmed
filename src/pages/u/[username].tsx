3ec38988ea77ec8274a9fe521e22bc24f6c8c92d
Marjan02 <kevinzevalja@gmail.com>
2023-05-17 18:43:17 +0700

    perfect loading

--- src/pages/u/[username].tsx
+++ src/pages/u/[username].tsx
@@ -2,65 +2,50 @@ import React from "react"
 import Image from "next/image"
 import { useRouter } from "next/router"
 import Layout from "@/components/Layout"
-import Heading from "@/components/ui/Heading"
 import Paragraph from "@/components/ui/Paragraph"
-import GetCollection, { GetSubCollection } from "@/utils/helpers"
+import { useGetCollection } from "@/utils/helpers"
 
 const UserWebPage = (): JSX.Element => {
   const router = useRouter()
   const { username } = router.query
-  const dataUsers = GetCollection("users")
-  let dataUser = dataUsers.find(
-    (document) => document.data().username == username
+  const { data: dataUsers, loading: usersLoading } = useGetCollection("users")
+  const { data: appSettings, loading: appSettingsLoading } = useGetCollection(
+    `users/${dataUsers[0]?.id}/app_settings`
   )
-  // console.log("bang", dataUser);
-  const appSetting = GetSubCollection(`users/${dataUser?.id}/app_settings`)[0]
-  // console.log("links", links);
+  const isLoading = usersLoading || appSettingsLoading
+
+  const dataUser = dataUsers.find((document) => document.username === username)
+  const appSetting = appSettings[0]
+
   return (
     <Layout>
-      <div className="mt-10 flex h-96 flex-col items-center justify-center">
-        {/* {links.map((document, i) => ( */}
-        <>
-          <p>{appSetting?.data()?.font_family}</p>
-        </>
-        {/* ))} */}
-        <Image
-          src={dataUser?.data().photoURL}
-          className="rounded-full"
-          alt="Photo Profile"
-          width={100}
-          height={100}
-        />
-        <Paragraph className="mt-3 font-semibold">
-          {dataUser?.data().username}
-        </Paragraph>
-        <Paragraph className="text-[9px] text-gray-400">
-          {dataUser?.data().bio}
-        </Paragraph>
-      </div>
-      {/* {dataUsers.map((document, i) => {
-        if (document.data().username == username && !isUserFound) {
-            isUserFound = true;
-          return (
-              <header
-                className="flex flex-col justify-center items-center mt-10"
-                key={i}
-              >
-                <img
-                  src={document.data().photoURL}
-                  className="w-14 rounded-full"
-                  alt="Photo Profile"
-                />
-                <h3 className="font-semibold text-[.8rem] mt-3">
-                  {document.data().username}
-                </h3>
-                <small className="text-[9px] text-gray-400">
-                  {document.data().bio}
-                </small>
-              </header>
-          );
-        }
-      })} */}
+      {isLoading ? (
+        <div className="flex items-center justify-center h-96 flex-col gap-2">
+          <Paragraph>Loading...</Paragraph>
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 dark:border-white" />
+        </div>
+      ) : (
+        <div className="mt-10 flex h-96 flex-col items-center justify-center">
+          {/* {links.map((document, i) => ( */}
+          <>
+            <p>{appSetting?.font_family}</p>
+          </>
+          {/* ))} */}
+          <Image
+            src={dataUser?.photoURL}
+            className="rounded-full"
+            alt="Photo Profile"
+            width={100}
+            height={100}
+          />
+          <Paragraph className="mt-3 font-semibold">
+            {dataUser?.username}
+          </Paragraph>
+          <Paragraph className="text-[9px] text-gray-400">
+            {dataUser?.bio}
+          </Paragraph>
+        </div>
+      )}
     </Layout>
   )
 }

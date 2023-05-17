f46e9e5af2e0bfe3a67a8b3d9c2975a52dea3115
Marjan02 <kevinzevalja@gmail.com>
2023-05-17 18:49:01 +0700

    fix navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,11 +2,10 @@ import React from "react"
 import Link from "next/link"
 import { useAuth } from "@/context/AuthContext"
 import GetCollection from "@/utils/helpers"
-import { AlignJustify, Flower2, Menu, Smartphone } from "lucide-react"
+import { AlignJustify, Smartphone } from "lucide-react"
 
 import LogoutButton from "./LogoutButton"
 import ThemeSwitch from "./ThemeSwitch"
-import { Separator } from "./ui/separator"
 
 const Navigation = (): JSX.Element => {
   const { currentUser } = useAuth()
@@ -19,18 +18,29 @@ const Navigation = (): JSX.Element => {
     <div className="mx-auto max-w-5xl px-8">
       <div className="flex items-center justify-between py-6">
         <nav className="flex items-center gap-4">
-          <Link
-            className="py-4 pr-6 text-gray-900 dark:text-white flex items-center gap-2"
-            href="/"
-          >
-            <AlignJustify /> Links
-          </Link>
-          <Link
-            className="py-4 pr-6 text-gray-900 dark:text-white flex items-center gap-2"
-            href={`/u/${username}`}
-          >
-            <Smartphone /> Apperance
-          </Link>
+          {currentUser ? (
+            <>
+              <Link
+                className="py-4 pr-6 text-gray-900 dark:text-white flex items-center gap-2"
+                href="/"
+              >
+                <AlignJustify /> Links
+              </Link>
+              <Link
+                className="py-4 pr-6 text-gray-900 dark:text-white flex items-center gap-2"
+                href={`/u/${username}`}
+              >
+                <Smartphone /> Apperance
+              </Link>
+            </>
+          ) : (
+            <Link
+              className="py-4 pr-6 text-gray-900 dark:text-white flex items-center gap-2"
+              href="/"
+            >
+              <AlignJustify /> Create Your Links
+            </Link>
+          )}
         </nav>
         <nav className="flex items-center space-x-1">
           <LogoutButton />

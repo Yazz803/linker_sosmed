4e9783c251f6e01e672ecd2e2c847a77200ff13e
Muhammad Yazid Akbar <yazidakbar08@gmail.com>
2023-06-06 08:37:39 +0700

    fix sw redundant

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -71,19 +71,6 @@ export default function LinksPage() {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
           console.log("Notification permission granted");
-          if ("serviceWorker" in navigator) {
-            navigator.serviceWorker
-              .register("/firebase-messaging-sw.js")
-              .then(function (registration) {
-                console.log(
-                  "Registration successful, scope is:",
-                  registration.scope
-                );
-              })
-              .catch(function (err) {
-                console.log("Service worker registration failed, error:", err);
-              });
-          }
           const messaging = getMessaging(app);
           onMessage(messaging, (payload) => {
             console.log("Message received. ", payload);
